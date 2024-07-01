2894b7a59ec76a1da6ccc1e64164032035ae060f
Tetyana Pavlyuk <tetyana.pas@gmail.com>
2024-07-01 09:41:59 +0300

    Implementing a serializer through ModelSerializer inheritance. Add class Meta

--- author/serializers.py
+++ author/serializers.py
@@ -4,21 +4,13 @@ from author.models import Author
 
 
 class AuthorSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(read_only=True)
-    first_name = serializers.CharField(max_length=64)
-    last_name = serializers.CharField(max_length=64)
-    pseudonym = serializers.CharField(max_length=64, required=False)
-    age = serializers.IntegerField()
-    retired = serializers.BooleanField()
-
-    def create(self, validated_data):
-        return Author.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.first_name = Author.objects.get("first_name", instance.first_name)
-        instance.last_name = Author.objects.get("last_name", instance.last_name)
-        instance.pseudonym = Author.objects.get("pseudonym", instance.pseudonym)
-        instance.age = Author.objects.get("age", instance.age)
-        instance.retired = Author.objects.get("retired", instance.retired)
-        instance.save()
-        return instance
+    class Meta:
+        model = Author
+        fields = [
+            "id",
+            "first_name",
+            "last_name",
+            "pseudonym",
+            "age",
+            "retired",
+        ]
